e74bfc379ba984318c85f5ab567f1ae7b301b98b
Izmil-H <ismail.hosny111@gmail.com>
2021-11-21 20:32:30 -0500

    Initial commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import os
+from dotenv import load_dotenv
 
 from dmodge import Dmodge
 from dmoj_client import DmojClient
@@ -13,8 +14,10 @@ def setup(client):
     client.add_cog(ContestManager(client, dmoj_client))
 
 if __name__ == "__main__":
+    load_dotenv()
     DISCORD_CLIENT_SECRET = os.getenv('DISCORD_CLIENT_SECRET')
     client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
     setup(client)
     client.run(DISCORD_CLIENT_SECRET)
+
